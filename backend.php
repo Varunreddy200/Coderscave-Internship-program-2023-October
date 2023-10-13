617fa9cdb2bb54031427e0a619ebb06addb50d8e
Dasari Varun Reddy <100370259+Varunreddy200@users.noreply.github.com>
2023-10-13 20:04:33 +0530

    Add files via upload

---
+++ backend.php
@@ -0,0 +1,34 @@
+<?php
+// Define database connection parameters
+$servername = "localhost"; // Your MySQL server hostname
+$username = "your_username"; // Your MySQL username
+$password = "your_password"; // Your MySQL password
+$dbname = "your_database"; // Your MySQL database name
+
+// Create a connection to the MySQL database
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Handle form submission
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $password = password_hash($_POST["password"], PASSWORD_BCRYPT); // Hash the password for security
+
+    // Insert user data into the 'users' table
+    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+// Close the database connection
+$conn->close();
+?>
